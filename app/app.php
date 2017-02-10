5587a1a4a9be1433964954c328e34815dd69c0bb
Zach Swanson <zach.j.swanson@gmail.com>
2017-02-10 08:39:45 -0800

    create directory, basic silex routing, and initial twig template

---
+++ app/app.php
@@ -0,0 +1,22 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Contact.php';
+
+    session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    return $app;
+?>
